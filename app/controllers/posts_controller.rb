aaa9681983d4ade860961be8b84ad1cdc7a545c5
Abel Kalum <chumbabel@gmail.com>
2020-09-01 20:47:07 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,14 +22,10 @@ class PostsController < ApplicationController
     @post.update(post_params)
     redirect_to post_path(@post)
   end
- 
-  private
- 
-  def post_params
-    params.require(:post).permit(:title, :description)
-  end
 
 	def edit
 	  @post = Post.find(params[:id])
 	end
+	
+	
 end
